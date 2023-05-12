c5d12d91d4d0c0ce85863cba2f17d7912af7a050
pradeep kumar <pkumar@wm.edu>
2023-05-12 17:43:08 -0400

    added pybind11 submodule

--- csr.h
+++ csr.h
@@ -1,4 +1,11 @@
 #pragma once
 
 class graph_t {
+    public:
+        void init_graph();
+        void save_graph(const string& full_path);
+        void load_graph(const string& full_path);
+        void load_graph_noeid(const string& full_path) {
+        int get_vcount();
+        int get_ecount();
 };
